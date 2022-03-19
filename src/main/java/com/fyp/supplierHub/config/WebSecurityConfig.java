223bfa19b5393aaca3bd7e9db27a6433f72923f9
shahzaib <shahzaibalam231@gmail.com>
2022-03-19 12:05:24 +0500

    checking the bug

--- src/main/java/com/fyp/supplierHub/config/WebSecurityConfig.java
+++ src/main/java/com/fyp/supplierHub/config/WebSecurityConfig.java
@@ -55,4 +55,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+    // just checking the bug
 }
